3580ff45afb76a86036fd40eb7459c171d43cc50
Pavel-Kliukin <9213087@mail.ru>
2023-05-15 13:33:50 +0300

    Types display on cards done

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import './Card.css';
 import axios from "axios";
 
+
 function Card (props) {
 
   const [pokemon, setPokemon] = useState();
@@ -23,15 +24,15 @@ function Card (props) {
   return (
     <div className="card">
       <div className="idAndTypes">
-        <p className="pokID">{pokemon.id}</p>
+        <p className="pokID">#{pokemon.id}</p>
         <p className="pokTypes">{pokemon.types.map(element => 
-          <img className="pokemonTypeImg" src={"assets/"+element.type.name+".png"} alt={element.type.name}/>
+          <img className="pokemonTypeImg" src={`/assets/${element.type.name}.png`} alt={element.type.name}/>
           )}
         </p>
       </div>
       <img className="pokemonImg" src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
       <div className="bottomOfCard">
-        <p className="pokName">{pokemon.name}</p>  
+        <p className="pokName">{pokemon.name.toUpperCase()}</p>  
       </div>
     </div> 
   );
